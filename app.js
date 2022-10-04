1b404d1b86cb03ff06b826972c1821daa8015106
Wayd26 <wayedoun@gmail.com>
2022-10-04 15:19:41 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const favicon = require('serve-favicon')
+const { success, getUniqueId } = require('./helper.js')
+let pokemons = require('./mock-pokemon')
+
+const app = express()
+const port = 3003
+
+app
+    .use(favicon(__dirname + '/favicon.ico'))
+    .use(morgan('dev'))
+    .use(bodyParser.json())
+
+app.get('/', (req, res) => res.send('Hello world! 👋 '))
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    const message = 'Un pokémon a bien été trouvé.'
+    res.json(success(message, pokemon))
+})
+
+app.get('/api/pokemons', (req, res) => {
+    const message = 'La liste de pokémons a bien été retournée.'
+    res.json(success(message, pokemons))
+})
+
+app.post('/api/pokemons', (req, res) => {
+    const id = getUniqueId(pokemons)
+    const pokemonCreated = {...req.body, ...{id: id, created: new Date()}}
+    pokemons.push(pokemonCreated)
+    const message = `Le pokémon ${pokemonCreated.name} a bien été créé.`
+    res.json(success(message, pokemonCreated)) 
+})
+
+
+app.put('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const pokemonUpdated = { ...req.body, id: id }
+    pokemons = pokemons.map(pokemon => {
+     return pokemon.id === id ? pokemonUpdated : pokemon
+    })
+     
+    const message = `Le pokémon ${pokemonUpdated.name} a bien été modifié.`
+    res.json(success(message, pokemonUpdated))
+   });
+
+   app.delete('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemonDeleted = pokemons.find(pokemon => pokemon.id === id)
+    pokemons = pokemons.filter(pokemon => pokemon.id !== id)
+    const message = `Le pokémon ${pokemonDeleted.name} a bien été supprimé.`
+    res.json(success(message, pokemonDeleted))
+  });
+
+ 
+
+app.listen(port, () => console.log(`Notre application NodeJS est démarrée sur le port http://localhost:${port}`))
